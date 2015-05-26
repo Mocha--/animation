a003e76a82e4ed19cbe384bd0699292ca422b590
Mocha <xibowangmelb@gmail.com>
2015-05-26 16:00:47 +1000

    3232

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-
 "use strict"
 
+var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
 
-$(document).ready(function(){
-	$("#container").load("components/page1.html", function(){
-		console.log("page1 loaded")
-	})
+$(document).ready(function() {
+    $("#container").load("components/page1.html", function() {
+        $("#img2").hide()
+        $("#img3").hide()
+        $("#img1")
+            .addClass("animated flip")
+            .one(animationEnd, function() {
+                $(this)
+                    .removeClass("flip")
+                    .addClass("fadeOut")
+                    .one(animationEnd, function() {
+                        $("#img2")
+                            .show()
+                            .addClass("animated rollIn")
+                        $("#img3")
+                            .show()
+                            .addClass("animated bounceInRight")
+                    })
+            })
+    })
 })
