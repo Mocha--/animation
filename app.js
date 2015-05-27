1191a35a05e08abdfedcf91bf1f41996795b9cc7
Mocha <xibowangmelb@gmail.com>
2015-05-27 11:25:10 +1000

    3232

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
                     })
             })
     })*/
-
     $("#container").load("components/page3.html", function() {
         $("#img2").hide()
         $("#img3").hide()
@@ -57,3 +56,4 @@ $(document).ready(function() {
             })
     })
 })
+
