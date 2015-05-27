43177aa577d35e830421adc4c498905809aa1fcc
Michaeltym <1412909864@qq.com>
2015-05-27 11:23:30 +1000

    index

--- app.js
+++ app.js
@@ -3,57 +3,84 @@
 var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend"
 
 $(document).ready(function() {
-    /*$("#container").load("components/page1.html", function() {
-        $("#img2").hide()
-        $("#img3").hide()
-        $("#img1")
-            .addClass("animated flip")
-            .one(animationEnd, function() {
-                $(this)
-                    .removeClass("flip")
-                    .addClass("fadeOut")
-                    .one(animationEnd, function() {
-                        $("#img2")
-                            .show()
-                            .addClass("animated rollIn")
-                        $("#img3")
-                            .show()
-                            .addClass("animated bounceInRight")
-                    })
-            })
-    })*/
+    $("#img2").hide()
+    $("#img3").hide()
+    $("#img1")
+        .addClass("animated flip")
+        .one(animationEnd, function() {
+            $(this)
+                .removeClass("flip")
+                .addClass("fadeOut")
+                .one(animationEnd, function() {
+                    $("#img2")
+                        .show()
+                        .addClass("animated rollIn")
+                    $("#img3")
+                        .show()
+                        .addClass("animated bounceInRight")
+                })
+        })
+    $("#page1").on("swiperight", swipeDown)
+    $("#page2").on("swipeleft", swipeUp)
 
-    $("#container").load("components/page3.html", function() {
-        $("#img2").hide()
-        $("#img3").hide()
-        $("#img4").hide()
-        $("#img5").hide()
+    function swipeUp(event) {
+        window.location.href = "#page1"
+    }
 
-        $("#img6").addClass("animated flip")
-        $("#img7").addClass("animated bounceInRight")
-        $("#img8").addClass("animated bounceInLeft")
+    function swipeDown(event) {
+            window.location.href = "#page2"
+        }
+        /*$("#container").load("components/page1.html", function() {
+            $("#img2").hide()
+            $("#img3").hide()
+            $("#img1")
+                .addClass("animated flip")
+                .one(animationEnd, function() {
+                    $(this)
+                        .removeClass("flip")
+                        .addClass("fadeOut")
+                        .one(animationEnd, function() {
+                            $("#img2")
+                                .show()
+                                .addClass("animated rollIn")
+                            $("#img3")
+                                .show()
+                                .addClass("animated bounceInRight")
+                        })
+                })
+        })
+    
+        $("#container").load("components/page3.html", function() {
+            $("#img2").hide()
+            $("#img3").hide()
+            $("#img4").hide()
+            $("#img5").hide()
 
-        $("#img1")
-            .addClass("animated fadeInDownBig")
-            .one(animationEnd, function() {
-                $("#img2")
-                    .show()
-                    .addClass("animated fadeInDownBig")
-                    .one(animationEnd, function() {
-                        $("#img3")
-                            .show()
-                            .addClass("animated fadeInRightBig")
-                            .one(animationEnd, function() {
-                                $("#img4")
-                                    .show()
-                                    .addClass("animated fadeInRightBig")
-                                    .one(animationEnd, function() {
-                                        $("#img5")
-                                            .show()
-                                            .addClass("animated fadeInLeftBig")
-                                    })
-                            })
-                    })
-            })
-    })
+            $("#img6").addClass("animated flip")
+            $("#img7").addClass("animated bounceInRight")
+            $("#img8").addClass("animated bounceInLeft")
+
+            $("#img1")
+                .addClass("animated fadeInDownBig")
+                .one(animationEnd, function() {
+                    $("#img2")
+                        .show()
+                        .addClass("animated fadeInDownBig")
+                        .one(animationEnd, function() {
+                            $("#img3")
+                                .show()
+                                .addClass("animated fadeInRightBig")
+                                .one(animationEnd, function() {
+                                    $("#img4")
+                                        .show()
+                                        .addClass("animated fadeInRightBig")
+                                        .one(animationEnd, function() {
+                                            $("#img5")
+                                                .show()
+                                                .addClass("animated fadeInLeftBig")
+                                        })
+                                })
+                        })
+                })
+        })*/
 })
