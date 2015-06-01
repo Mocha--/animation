5f7dae6af48f481a23deac88c9195530f9b3aaef
Mocha <xibowangmelb@gmail.com>
2015-06-01 14:19:54 +1000

    3232

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-
 "use strict"
 
 format.extend(String.prototype)
 
-var createImg = function(src){
-	
+var createImg = function(src) {
+    return '<img src = {src} {name}>'.format({
+        "src": "hehehe",
+        "name": "lalala"
+    })
 }
 
 
 $(document).ready(function() {
-	console.log("{}".format("hehehe"))
-	var pages = aData.list
-	pages.forEach(function(page){
-		var elements = page.elements
-		elements.forEach(function(element){
-			var contentId = "#content" + page.num
+    console.log(createImg(11))
+    var pages = aData.list
+    pages.forEach(function(page) {
+        var elements = page.elements
+        elements.forEach(function(element) {
+            var contentId = "#content" + page.num
 
-			$(contentId).append("")
-		})
-	})
+            $(contentId).append("")
+        })
+    })
 })
