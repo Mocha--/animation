4c61fb38fb2add6dc4605305252196f2e6b4f3a0
Mocha <xibowangmelb@gmail.com>
2015-06-05 10:47:07 +1000

    323232

--- app.js
+++ app.js
@@ -36,79 +36,79 @@ var createImg = function(opt) {
 }
 
 var createDiv = function(opt) {
-        var id = 'id = "{}"'.format(opt.id)
-        if (opt.dataRole) {
-            var dataRole = 'data-role = "{}"'.format(opt.dataRole)
+    var id = 'id = "{}"'.format(opt.id)
+    if (opt.dataRole) {
+        var dataRole = 'data-role = "{}"'.format(opt.dataRole)
 
-            var innerHTML = opt.innerHTML
-            if (opt.style) {
-                if (opt.style.left)
-                    opt.style.left = opt.style.left.toString() + "px"
-                if (opt.style.right)
-                    opt.style.right = opt.style.right.toString() + "px"
-                if (opt.style.top)
-                    opt.style.top = opt.style.top.toString() + "px"
-                if (opt.style.bottom)
-                    opt.style.bottom = opt.style.bottom.toString() + "px"
-                if (opt.style.height)
-                    opt.style.height = opt.style.height.toString() + "px"
-                if (opt.style.width)
-                    opt.style.width = opt.style.width.toString() + "px"
-                var str = JSON.stringify(opt.style).replace(/,/g, ";").replace(/"/g, "").replace(/\{/, "").replace(/\}/, "")
-                var style = 'style = "{}"'.format(str)
-            }
-            return "<div {dataRole} {id} {style}> {innerHTML} </div>".format({
-                "id": id,
-                "dataRole": dataRole,
-                "style": style,
-                "innerHTML": innerHTML
-            })
-
+        var innerHTML = opt.innerHTML
+        if (opt.style) {
+            if (opt.style.left)
+                opt.style.left = opt.style.left.toString() + "px"
+            if (opt.style.right)
+                opt.style.right = opt.style.right.toString() + "px"
+            if (opt.style.top)
+                opt.style.top = opt.style.top.toString() + "px"
+            if (opt.style.bottom)
+                opt.style.bottom = opt.style.bottom.toString() + "px"
+            if (opt.style.height)
+                opt.style.height = opt.style.height.toString() + "px"
+            if (opt.style.width)
+                opt.style.width = opt.style.width.toString() + "px"
+            var str = JSON.stringify(opt.style).replace(/,/g, ";").replace(/"/g, "").replace(/\{/, "").replace(/\}/, "")
+            var style = 'style = "{}"'.format(str)
         }
-
-        $(document).ready(function() {
-            var pages = aData.list
-            var pageStr = ""
-            pages.forEach(function(page) {
-                var elements = page.elements
-                var eleStr = ""
-                elements.forEach(function(element) {
-                    var imgDIV = createImg({
-                        "src": element.properties.src,
-                        "imgsrc": element.properties.imgSrc,
-                        "style": element.properties.imgStyle
-                    })
-                    var elementDIV = createDiv({
-                        "id": null,
-                        "style": element.css,
-                        "innerHTML": imgDIV
-                    })
-                    eleStr += elementDIV
-                })
-                var contentDIV = createDiv({
-                    "innerHTML": eleStr,
-                    "dataRole": "content"
-                })
-                var pageDIV = createDiv({
-                    "id": "page" + page.num,
-                    "innerHTML": contentDIV,
-                    "dataRole": "page"
-                })
-                pageStr += pageDIV
-            })
-            $("#container").append(pageStr)
-
-            $("#container").on("swipeleft", function() {
-                if (curPage <= MAXPAGE - 1) {
-                    curPage += 1
-                }
-                window.location.href = "#page" + curPage.toString()
-            })
-
-            $("#container").on("swiperight", function() {
-                if (curPage >= 2) {
-                    curPage -= 1
-                }
-                window.location.href = "#page" + curPage.toString()
-            })
+        return "<div {dataRole} {id} {style}> {innerHTML} </div>".format({
+            "id": id,
+            "dataRole": dataRole,
+            "style": style,
+            "innerHTML": innerHTML
         })
+    }
+}
+
+$(document).ready(function() {
+    var pages = aData.list
+    var pageStr = ""
+    pages.forEach(function(page) {
+        var elements = page.elements
+        var eleStr = ""
+        elements.forEach(function(element) {
+            var imgDIV = createImg({
+                "src": element.properties.src,
+                "imgsrc": element.properties.imgSrc,
+                "style": element.properties.imgStyle
+            })
+            var elementDIV = createDiv({
+                "id": null,
+                "style": element.css,
+                "innerHTML": imgDIV
+            })
+            eleStr += elementDIV
+        })
+        var contentDIV = createDiv({
+            "innerHTML": eleStr,
+            "dataRole": "content"
+        })
+        var pageDIV = createDiv({
+            "id": "page" + page.num,
+            "innerHTML": contentDIV,
+            "dataRole": "page"
+        })
+        pageStr += pageDIV
+    })
+    $("#container").append(pageStr)
+
+    $("#container").on("swipeleft", function() {
+        if (curPage <= MAXPAGE - 1) {
+            curPage += 1
+        }
+        window.location.href = "#page" + curPage.toString()
+    })
+
+    $("#container").on("swiperight", function() {
+        if (curPage >= 2) {
+            curPage -= 1
+        }
+        window.location.href = "#page" + curPage.toString()
+    })
+})
