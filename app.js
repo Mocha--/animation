31dcfae6198f05133fc851d51735c794a6de9aa0
Mocha <xibowangmelb@gmail.com>
2015-06-05 14:33:43 +1000

    very very fucking you yi yi

--- app.js
+++ app.js
@@ -5,13 +5,51 @@ format.extend(String.prototype)
 var curPage = 1
 var MAXPAGE = aData.list.length
 
+var aniMap = []
+
 var createImg = function(opt) {
     if (opt.src) {
         var src = 'src = "{}"'.format(opt.src)
     } else if (opt.imgsrc) {
         var src = 'src = "{}"'.format(opt.imgsrc)
     }
-    console.log(opt)
+    if (opt.style) {
+        if (opt.style.left)
+            opt.style.left = opt.style.left.toString() + "px"
+        if (opt.style.right)
+            opt.style.right = opt.style.right.toString() + "px"
+        if (opt.style.top)
+            opt.style.top = opt.style.top.toString() + "px"
+        if (opt.style.bottom)
+            opt.style.bottom = opt.style.bottom.toString() + "px"
+        if (opt.style.height)
+            opt.style.height = opt.style.height.toString() + "px"
+        if (opt.style.width)
+            opt.style.width = opt.style.width.toString() + "px"
+        var str = JSON.stringify(opt.style).replace(/,/g, ";").replace(/"/g, "").replace(/\{/, "").replace(/\}/, "")
+        var style = 'style = "{}"'.format(str)
+    } else if (opt.imgsrc) {
+        var str = "cover"
+        var style = 'style = "{}"'.format(str)
+    }
+
+    return "<img {src} {style}>".format({
+        "src": src,
+        "style": style
+    })
+}
+
+var createDiv = function(opt) {
+    var id = 'id = "{}"'.format(opt.id)
+    if (opt.dataRole) {
+        var dataRole = 'data-role = "{}"'.format(opt.dataRole)
+    }
+
+    if (opt.class) {
+        var cla = ' class = "{}"'.format(opt.class)
+    }
+
+    var innerHTML = opt.innerHTML
     if (opt.style) {
         if (opt.style.left)
             opt.style.left = opt.style.left.toString() + "px"
@@ -28,46 +66,13 @@ var createImg = function(opt) {
         var str = JSON.stringify(opt.style).replace(/,/g, ";").replace(/"/g, "").replace(/\{/, "").replace(/\}/, "")
         var style = 'style = "{}"'.format(str)
     }
-    else if(opt.imgsrc){
-        var str = "cover"
-        var style = 'style = "{}"'.format(str)
-    }
-
-    return "<img {src} {style}>".format({
-        "src": src,
-        "style": style
+    return "<div {id} {class} {dataRole} {style}> {innerHTML} </div>".format({
+        "id": id,
+        "class": cla,
+        "dataRole": dataRole,
+        "style": style,
+        "innerHTML": innerHTML
     })
-}
-
-var createDiv = function(opt) {
-    var id = 'id = "{}"'.format(opt.id)
-    if (opt.dataRole) {
-        var dataRole = 'data-role = "{}"'.format(opt.dataRole)
-
-        var innerHTML = opt.innerHTML
-        if (opt.style) {
-            if (opt.style.left)
-                opt.style.left = opt.style.left.toString() + "px"
-            if (opt.style.right)
-                opt.style.right = opt.style.right.toString() + "px"
-            if (opt.style.top)
-                opt.style.top = opt.style.top.toString() + "px"
-            if (opt.style.bottom)
-                opt.style.bottom = opt.style.bottom.toString() + "px"
-            if (opt.style.height)
-                opt.style.height = opt.style.height.toString() + "px"
-            if (opt.style.width)
-                opt.style.width = opt.style.width.toString() + "px"
-            var str = JSON.stringify(opt.style).replace(/,/g, ";").replace(/"/g, "").replace(/\{/, "").replace(/\}/, "")
-            var style = 'style = "{}"'.format(str)
-        }
-        return "<div {dataRole} {id} {style}> {innerHTML} </div>".format({
-            "id": id,
-            "dataRole": dataRole,
-            "style": style,
-            "innerHTML": innerHTML
-        })
-    }
 }
 
 $(document).ready(function() {
@@ -84,6 +89,7 @@ $(document).ready(function() {
             })
             var elementDIV = createDiv({
                 "id": null,
+                "class": "imgDiv",
                 "style": element.css,
                 "innerHTML": imgDIV
             })
